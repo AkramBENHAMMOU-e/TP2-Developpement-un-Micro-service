65b38d6964c4ef70a3922f2aa61458ff469b2f42
Akram BENHAMMOU <aben200244@gmail.com>
2025-10-11 15:30:14 +0200

    Generation et test de la doc swagger , et exposition les apis restfuls en utilisant spring data rest

--- src/main/java/com/tp/bankaccservice/repositories/BankAccountRepository.java
+++ src/main/java/com/tp/bankaccservice/repositories/BankAccountRepository.java
@@ -2,6 +2,8 @@ package com.tp.bankaccservice.repositories;
 
 import com.tp.bankaccservice.entities.BankAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface BankAccountRepository extends JpaRepository<BankAccount, String> {
 }
