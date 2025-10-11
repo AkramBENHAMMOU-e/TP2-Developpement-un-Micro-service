514c8c57d1654f1184e441c0365f317fbacd2e82
Akram BENHAMMOU <aben200244@gmail.com>
2025-10-11 12:16:03 +0200

    initilalisation du projet spring boot avec les dependances web , lombok, spring data,h2, spring for graphQl

---
+++ src/main/java/com/tp/bankaccservice/BankAccServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tp.bankaccservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAccServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankAccServiceApplication.class, args);
+    }
+
+}
