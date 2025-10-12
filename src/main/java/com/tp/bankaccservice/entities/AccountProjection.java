0a5e51ec9af7dbbfcbafaa0a0fa110e18bc7cc8d
Akram BENHAMMOU <aben200244@gmail.com>
2025-10-12 12:53:28 +0200

    tester les projections avec @Projection de spring data rest

---
+++ src/main/java/com/tp/bankaccservice/entities/AccountProjection.java
@@ -0,0 +1,9 @@
+package com.tp.bankaccservice.entities;
+
+import org.springframework.data.rest.core.config.Projection;
+
+@Projection(types = BankAccount.class, name = "p1")
+public interface AccountProjection {
+    public String getId();
+    public Double getBalance();
+}
--- src/main/java/com/tp/bankaccservice/repositories/BankAccountRepository.java
+++ src/main/java/com/tp/bankaccservice/repositories/BankAccountRepository.java
@@ -1,9 +1,17 @@
 package com.tp.bankaccservice.repositories;
 
 import com.tp.bankaccservice.entities.BankAccount;
+import com.tp.bankaccservice.enums.AccountType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.List;
 
 @RepositoryRestResource
 public interface BankAccountRepository extends JpaRepository<BankAccount, String> {
+
+    @RestResource(path = "byType")
+    List<BankAccount> findByType(@Param("t") AccountType type);
 }
